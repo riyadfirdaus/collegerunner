6d4b1be9183836f89fe28fb95c93988d4212c1c6
riyadfirdaus <56018683+riyadfirdaus@users.noreply.github.com>
2021-01-04 07:58:22 +0700

    Edit Directory

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    int a = 10, b = 20;
+    float flo = 1.123f;
+    string s = "sata mau makan";
+    bool fact = true;
+
+    // Start is called before the first frame update
+    void start()
+    {
+        if(fact == true)
+        {
+            print(a.ToString() + "b = " + b.ToString() + s + flo.ToString());
+        }
+    }
+}
